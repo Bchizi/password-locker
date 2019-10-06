786519230a677194421dfc5b5dfaa24b8f7fcede
bchizi <leonbichanga@gmail.com>
2019-10-06 23:36:41 +0300

    added instance variables to the class and initialised them

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    pass
+ 
+
+def __init__(self,firstname,lastname,password):
+
+    #INSTANCE VARIABLES
+    self.firstname = firstname
+    self.lastname = lastname
+    self.password = password
+    
+
