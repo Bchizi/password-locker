77d0d0315ef00903f904a114e2cee47b9eacd155
bchizi <leonbichanga@gmail.com>
2019-10-06 23:39:58 +0300

    added a class  variable

--- user.py
+++ user.py
@@ -1,12 +1,15 @@
 class User:
-    pass
  
-
-def __init__(self,firstname,lastname,password):
+ 
+ user_list = []
+ 
+ def __init__(self,firstname,lastname,password):
 
     #INSTANCE VARIABLES
     self.firstname = firstname
     self.lastname = lastname
     self.password = password
-    
+
+
+
 
