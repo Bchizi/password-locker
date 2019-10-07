8ffae810174bd26424c6cbda71e7508b3cea66c1
bchizi <leonbichanga@gmail.com>
2019-10-07 16:47:51 +0300

    add saving credentials method to the module and also makinga test for it

--- user.py
+++ user.py
@@ -1,26 +1,39 @@
 class User:
  
  
- user_list = []
+    user_list = []
 
- def save_user(self):
+    def save_user(self):
      """
      save user method saves user objects into user_list
      """
 
      User.user_list.append(self)
 
-
- 
- def __init__(self,firstname,lastname,username,password):
-
-    #INSTANCE VARIABLES
-    self.firstname = firstname
-    self.lastname = lastname
-    self.username = username
-    self.password = password
     
+ 
+    def __init__(self,firstname,lastname,username,password):
+
+        #INSTANCE VARIABLES
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password
 
 
+class Credentials:
 
+    credentials_list = []
+    def save_credentials(self):
+        Credentials.credentials_list.append(self)
 
+    def delete_user(self):
+        ''' 
+        delets the user
+        '''
+        Credentials.credentials_list.remove(self)
+
+    def __init__(self,social_account,social_username,social_password):
+        self.social_account = social_account
+        self.social_username = social_username
+        self.social_password = social_password
--- user_test.py
+++ user_test.py
@@ -1,27 +1,63 @@
 import unittest
 from user import User
+from user import Credentials
+
 
 class TestUser(unittest.TestCase):
 
-    def setUp (self):
+    def setUp(self):
 
-        self.new_account = User ("leon","bichanga","bchizi","lehann")
+        self.new_account = User("leon", "bichanga", "bchizi", "lehann")
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
 
     def test_init(self):
 
-        self.assertEqual(self.new_account.firstname,"leon")
-        self.assertEqual(self.new_account.lastname,"bichanga")
-        self.assertEqual(self.new_account.username,"bchizi")
-        self.assertEqual(self.new_account.password,"lehann")
+        self.assertEqual(self.new_account.firstname, "leon")
+        self.assertEqual(self.new_account.lastname, "bichanga")
+        self.assertEqual(self.new_account.username, "bchizi")
+        self.assertEqual(self.new_account.password, "lehann")
 
     def test_save_user(self):
         self.new_account.save_user()
-        self.assertEqual(len(User.user_list),1)
-    
+        self.assertEqual(len(User.user_list), 1)
+
     def test_save_multiple_users(self):
-        test_user = User("chris","jaire","chrisj","jaish")
+        self.new_account.save_user()
+        test_user = User("chris", "jaire", "chrisj", "jaish")
         test_user.save_user()
-        self.assertEqual(len(User.user_list),2)
+        self.assertEqual(len(User.user_list), 2)
+
+
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+       self.new_account = Credentials ("instagram","bchizi","lehann")
+    
+    def tearDown(self):
+        Credentials.credentials_list=[]
+
+    def test_credential_init(self):
+        self.assertEqual(self.new_account.social_account,"instagram")
+        self.assertEqual(self.new_account.social_username,"bchizi")
+        self.assertEqual(self.new_account.social_password,"lehann")
+
+    
+
+    def test_delete_user(self):
+        '''
+        test_delete_contact to test if we can remove a contact from our contact list
+        '''
+        self.new_account.save_credentials()  
+        test_user = User("Test", "user", "0712345678", 
+                         "test@user.com")  # new contact
+        test_user.save_user()
+        self.new_account.delete_user()  # deleting a user object
+        self.assertEqual(len(User.user_list), 1)
         
 
 
