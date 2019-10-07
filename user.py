00516d0d4b0176360ead8285a9063cab7eb7726f
bchizi <leonbichanga@gmail.com>
2019-10-07 09:34:46 +0300

    added the save and confirmed that it is working

--- user.py
+++ user.py
@@ -2,12 +2,22 @@ class User:
  
  
  user_list = []
+
+ def save_user(self):
+     """
+     save user method saves user objects into user_list
+     """
+
+     User.user_list.append(self)
+
+
  
- def __init__(self,firstname,lastname,password):
+ def __init__(self,firstname,lastname,username,password):
 
     #INSTANCE VARIABLES
     self.firstname = firstname
     self.lastname = lastname
+    self.username = username
     self.password = password
 
 
--- user_test.py
+++ user_test.py
@@ -5,13 +5,18 @@ class TestUser(unittest.TestCase):
 
     def setUp (self):
 
-        self.new_account = User ("leon","bichanga","lehann")
+        self.new_account = User ("leon","bichanga","bchizi","lehann")
 
     def test_init(self):
-        
+
         self.assertEqual(self.new_account.firstname,"leon")
         self.assertEqual(self.new_account.lastname,"bichanga")
+        self.assertEqual(self.new_account.username,"bchizi")
         self.assertEqual(self.new_account.password,"lehann")
+
+    def test_save_user(self):
+        self.new_account.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == '__main__':
     unittest.main()
